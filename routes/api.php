6dd29a34d5182f16bfca64955c712f34b934da46
Mona <moon.2060@gmail.com>
2024-08-25 22:57:34 +0300

    'fix routes'

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,9 @@ use App\Http\Controllers\Api\RatingController;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you can register API routes for your application.
-| Routes are loaded by the RouteServiceProvider and assigned to the "api"
-| middleware group. Here, we define routes for movie management, rating
-| management, and user authentication.
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
@@ -23,7 +22,6 @@ Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () {
     /**
      * Store a new movie.
      *
-     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     Route::post('movies', [MovieController::class, 'store']);
@@ -31,7 +29,6 @@ Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () {
     /**
      * Update an existing movie.
      *
-     * @param \Illuminate\Http\Request $request
      * @param \App\Models\Movie $movie
      * @return \Illuminate\Http\JsonResponse
      */
@@ -49,19 +46,33 @@ Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () {
 // Routes that require user access
 Route::group(['middleware' => ['auth:sanctum', 'role:user']], function () {
     /**
-     * Handle ratings related actions for movies.
+     * Store a new rating for a movie.
      *
-     * Provides routes to store, update, and delete ratings for movies.
-     * 
-     * @return \Illuminate\Routing\Router
+     * @param int $movieId
+     * @return \Illuminate\Http\JsonResponse
      */
-    Route::apiResource('/movies/{movieId}/rating', RatingController::class)
-        ->only(['store', 'update', 'destroy']);
+    Route::post('/movies/{movieId}/rating', [RatingController::class, 'store']);
+
+    /**
+     * Update an existing rating for a movie.
+     *
+     * @param int $movieId
+     * @return \Illuminate\Http\JsonResponse
+     */
+    Route::put('/movies/{movieId}/rating', [RatingController::class, 'update']);
+
+    /**
+     * Delete a rating for a movie.
+     *
+     * @param int $movieId
+     * @return \Illuminate\Http\JsonResponse
+     */
+    Route::delete('/movies/{movieId}/rating', [RatingController::class, 'destroy']);
 });
 
 // Public routes for movies
 Route::apiResource('movies', MovieController::class)
-    // Exclude store, update, and destroy methods from the resource routes
+    // Exclude store, update, and destroy methods from apiResource routes
     ->except('store', 'update', 'destroy');
 
 /**
